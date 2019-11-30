f84ced3bf13be2389af587a0dbcdb543eebd1de7
dev@MiniBuntu <r.mgoyan@finik.pro>
2019-11-30 12:50:58 +0200

    add scale from hub

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# demo
+# demo Selenide tests
--- RunTests.sh
+++ RunTests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker-compose -f SeleniumHub.yml up --scale chrome=4 -d
+docker-compose -f SeleniumHub.yml up --scale chrome=1 -d
 mvn clean test -Denv $1
 errorCode=$? # save the exit code as the first thing done in the trap function
 docker-compose -f SeleniumHub.yml down
